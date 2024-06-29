43e578daa39d6f555e908a880c028d8dddcf1d2f
Adriel Alejandro Aliaga Benavides <aaliagab85@gmail.com>
2024-06-28 22:55:38 -0400

    Initial commit

---
+++ InvoiceExtract.py
@@ -0,0 +1,42 @@
+from openai import OpenAI
+import json
+import os
+from urllib.parse import urlparse
+
+client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
+def load_json_schema(schema_file: str) -> dict:
+    with open(schema_file, 'r') as file:
+        return json.load(file)
+
+image_url = 'https://www.invoicesimple.com/wp-content/uploads/2018/06/Sample-Invoice-printable.png'
+
+
+invoice_schema = load_json_schema('invoice_schema.json')
+response = client.chat.completions.create(
+    model='gpt-4o',
+    response_format={"type": "json_object"},
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "provide JSON file that represents this document. Use this JSON Schema: " +
+                    json.dumps(invoice_schema)},
+                {
+                    "type": "image_url",
+                    "image_url": {"url": image_url}
+                }
+            ],
+        }
+    ],
+    max_tokens=500,
+)
+print(response.choices[0].message.content)
+json_data = json.loads(response.choices[0].message.content)
+filename_without_extension = os.path.splitext(os.path.basename(urlparse(image_url).path))[0]
+json_filename = f"{filename_without_extension}.json"
+
+with open(json_filename, 'w') as file:
+    json.dump(json_data, file, indent=4)
+
+print(f"JSON data saved to {json_filename}")
